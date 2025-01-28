fa7e2f581163a4035fbfde6f81f5a9828899f950
Caitlin Lehman <caitlin.lehman@kellogg.northwestern.edu>
2025-01-28 18:01:20 +0000

    creates SQL statement for movies output

--- kmdb.sql
+++ kmdb.sql
@@ -321,14 +321,15 @@ INSERT INTO roles (
 
 -- The SQL statement for the movies output
 -- TODO!
-
+SELECT movies.movie_title, movies.year, movies.mpaa_rating, studios.studio_name 
+FROM movies
+INNER JOIN studios ON movies.studio_id = studios.id
 
 -- Prints a header for the cast output
 .print ""
 .print "Top Cast"
 .print "========"
 .print ""
-
 
 -- The SQL statement for the cast output
 -- TODO!
